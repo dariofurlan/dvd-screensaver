5deb511959a2963fd05a565564ac4c22644b8354
Dario Furlan <dario.furlan.51@gmail.com>
2018-12-23 16:23:42 +0100

    Almost completed

---
+++ dvd_screensaver.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -jar JavaFXApp.jar $1
--- src/dvd_screensaver/Main.java
+++ src/dvd_screensaver/Main.java
@@ -2,11 +2,8 @@ package dvd_screensaver;
 
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.CacheHint;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -16,46 +13,33 @@ import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
 import java.io.File;
+import java.io.InputStream;
 
 public class Main extends Application {
 
     private static final double img_width = 300;
     private static final double img_height = img_width / 1.9;
-    private int last_img_n = (int) (Math.random()*7);
+    private int last_img_n = (int) (Math.random() * 7);
 
     private ImageView imageView;
     private Image[] images;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        /*Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Scene scene = new Scene(root);
-        scene.setCursor(Cursor.NONE);
-        scene.setFill(Color.GREY);
-        scene.fillProperty();
-        scene.
-        primaryStage.setFullScreen(true);
-        primaryStage.setResizable(false);
-        primaryStage.setTitle("DVD Screensaver");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        primaryStage.setX(0);
-        primaryStage.setY(0);*/
-
-
-
+    public void start(Stage primaryStage) {
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds();
         double width = primaryScreenBounds.getWidth();
         double height = primaryScreenBounds.getHeight();
 
-        Canvas canvas = new Canvas(width, height);
-        String current = new File(".").getCanonicalPath();
-        System.out.println(current);
         images = new Image[8];
+        String file_path = "/";
+        for (int i = 0; i <= 7; i++) {
 
-        for (int i=0;i<=7;i++) {
-            images[i] = new Image("file:dvdlogo_"+i+".png", img_width, img_height, false, false);
+            String file_name = "dvdlogo_" + i + ".png";
+            System.out.println(file_path + file_name);
+            images[i] = new Image(getClass().getResourceAsStream(file_path + file_name), img_width, img_height, false, false);
+            System.out.println(images[i].isError());
         }
 
         Pane root = new Pane();
@@ -63,9 +47,9 @@ public class Main extends Application {
         root.setMinHeight(height);
 
         imageView = new ImageView();
+        imageView.setImage(images[0]);
         root.getChildren().add(imageView);
 
-        imageView.setImage(images[0]);
 
         primaryStage.setFullScreen(true);
         primaryStage.setResizable(false);
@@ -103,18 +87,18 @@ public class Main extends Application {
                 }
 
                 //top or bottom invert Y direction
-                if (y<0 || y+img_height>=height) {
-                    direction_y *=-1;
+                if (y < 0 || y + img_height >= height) {
+                    direction_y *= -1;
                     bounce();
                 }
                 //left or right invert X direction
-                if (x<0 || x+img_width>=width) {
-                    direction_x *=-1;
+                if (x < 0 || x + img_width >= width) {
+                    direction_x *= -1;
                     bounce();
                 }
 
-                x+=increment_x*direction_x;
-                y+=increment_y*direction_y;
+                x += increment_x * direction_x;
+                y += increment_y * direction_y;
 
                 imageView.setX(x);
                 imageView.setY(y);
@@ -123,9 +107,28 @@ public class Main extends Application {
     }
 
 
-
     public static void main(String[] args) {
-        launch(args);
+        if (args != null && args.length > 0) {
+            String cmd = args[0];
+            switch (cmd) {
+                case "/c":
+                    // settings dialog box
+
+                    break;
+                case "/p":
+                    // preview the screensaver
+
+                    break;
+                case "/s":
+                    // run the screensaver
+                    launch();
+                    break;
+                default:
+                    // unsupported
+                    break;
+            }
+        }
+        System.exit(0);
     }
 
     private void bounce() {
